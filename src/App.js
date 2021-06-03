ba41a5e6c3ad5eae96b9905da41bfd07d0494cb7
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-06-03 14:32:10 -0400

    Fix file not found error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test 231312313
+          Learn React
         </a>
       </header>
     </div>
