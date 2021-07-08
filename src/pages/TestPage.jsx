4e4c8ef94cd7b326c4339b2021697517a334c14c
Nausher Rao <SherRao15@gmail.com>
2021-07-08 13:19:43 -0400

    Add WIP test file for Spotify API as well as remove TestPage code

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,31 +1,15 @@
-import React from "react";
-import { TestOrganismComponent } from "@organisms";
+import React, {useState, useEffect} from "react";
 import SpotifyWebApi from "spotify-web-api-node";
 
 function TestPage() {
-    let response;
-    test().then(data => response = data);
-
     return (
         <div>
-            <p>hey</p>
-            {response}
-            <TestOrganismComponent />
-
+           
         </div>
     );
 }
 
+
 export default TestPage;
 
-async function test() {
-    const api = new SpotifyWebApi(  
-    );
-
-    console.log("Testing test()");
-    const response = await api.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE");
-    console.log(response.body);
-    return response.body;
-
-}
     
