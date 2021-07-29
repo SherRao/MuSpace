58f0ab9f8d7ca14bb290e4dc68a2f6c476c0194a
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-07-29 16:04:36 -0400

    Fix black background for Unprotected pages (login, register, verify)
    Change font sizes and similar stuffs aswell

--- src/misc/UnprotectedRoute.jsx
+++ src/misc/UnprotectedRoute.jsx
@@ -2,7 +2,22 @@ import React, { Component } from "react";
 import Styled from "styled-components";
 import { Route, Redirect } from "react-router-dom";
 
+const Center = Styled.div`
+    width: 100%;
+    min-height: 100vh;
+    height: max-content;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: sticky;
+
+    background-color: ${props => props.theme.colors.black};
+`;
+
 function UnprotectedRoute({ component, isLoggedIn, isVerified, ...rest }) {
+    const Component = component;
     if (isLoggedIn && isVerified) {
         return (
             <Route {...rest}>
@@ -20,7 +35,9 @@ function UnprotectedRoute({ component, isLoggedIn, isVerified, ...rest }) {
     } else {
         return (
             <Route {...rest}>
-                {component}
+                <Center>
+                    <Component />
+                </Center>
             </Route>
         );
     }
