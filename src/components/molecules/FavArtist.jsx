dbb3b463b5f8bc773b0f15f568264571b01ceede
Robert336 <rmazza33@gmail.com>
2021-07-29 16:13:53 -0400

    create FavArtist card for the home page

--- src/components/molecules/FavArtist.jsx
+++ src/components/molecules/FavArtist.jsx
@@ -4,29 +4,56 @@ import { AlbumCover, Card } from "@atoms";
 
 
 const StyledDiv = Styled.div`
-    background-color: ${props => props.theme.colors.white};
+    max-width: 30em;
+    height: 10em;
     
-    width: 30%;
-    height: 10%;
-    
-    display: grid;
-    grid-template-columns: 20vw auto;
-    grid-auto-rows: 10vh auto;
+    display: flex;
+    flex-flow: row wrap;
+    flex-direction: row;
+    align-items: center;
 
     transition: all 0.25s ease;
 
-    background-color: linear-gradient(89.84deg, rgba(0, 194, 255, 0.7) 0.12%, rgba(177, 13, 255, 0.7) 99.86%);
+    background: linear-gradient(89.84deg, rgba(0, 194, 255, 0.7) 0.12%, rgba(177, 13, 255, 0.7) 99.86%);
     border-radius: 20px;
 
-    -webkit-box-sizing: border-box; /* Safari/Chrome, other WebKit */
-    -moz-box-sizing: border-box;    /* Firefox, other Gecko */
-    box-sizing: border-box;         /* Opera/IE 8+ */
 `;
 
-function FavArtist() {
+const Banner = Styled.div`
+    background-color: grey;
+    height: 2em;
+    width: 100%;
+    border-radius: 15px 15px 0px 0px;
+
+`;
+
+
+const TextDiv = Styled.div`
+    display: flex;
+    flex-direction: column;
+
+`;
+
+const ArtistName = Styled.div`
+    font-size: ${props => props.theme.fontSizes.large};
+
+`;
+
+const HoursPlayed = Styled.div`
+    font-size: ${props => props.theme.fontSizes.medium};
+`;
+
+
+
+function FavArtist({artist_name, hours_played}) {
     return (
         <StyledDiv>
-            
+            <Banner/>
+            <AlbumCover width="6em"/>
+            <TextDiv>
+                <ArtistName>{artist_name}</ArtistName>
+                <HoursPlayed>{hours_played}</HoursPlayed>
+            </TextDiv>
         </StyledDiv>
     );
 }
