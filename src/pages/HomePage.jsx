dc0c2d8a449f68383290e33576970b112b91b188
Robert336 <rmazza33@gmail.com>
2021-08-05 15:26:59 -0400

    Refactor FavArtist to FavCard
    Changing the FavArtist to FavCard for universal use of the "Your fav artist of the week" and the "Your favourite song of the week" card only needing one component.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Styled from "styled-components";
-import {FavArtist} from "@molecules";
+import {FavCard} from "@molecules";
 
 const Container = Styled.div`
     width: 100%;
     height: 100%;
-    padding: 5em;
+    padding: 1em;
 
     margin: 0;
     top: 0;
@@ -16,6 +16,7 @@ const Container = Styled.div`
     display: inline-flex;
     flex-direction: column;
     align-items: center;
+    
     transition: all 0.25s ease;
     background-color: ${props => props.theme.colors.black};
 
@@ -27,8 +28,8 @@ const Container = Styled.div`
 function HomePage() {
     return (
         <Container>
-            <FavArtist artist_name="Lil Uzi Vert" hours_played="10" artist_pic_url="https://i.scdn.co/image/ab676161000051749cc6d44767dda18ee4e1be9f"/>
-            <h1>Hello world</h1>
+            <FavCard card_title="Your favourite artist of the week" main_text="Lil Uzi Vert" hours_played="10" pic_url="https://i.scdn.co/image/ab676161000051749cc6d44767dda18ee4e1be9f"/>
+            <FavCard card_title="Your favourite song of the week" main_text="Hotel California" sub_text="Eagles" hours_played="1" pic_url="https://i.scdn.co/image/ab67616d0000b2734637341b9f507521afa9a778"/>
         </Container>
     );
 }
